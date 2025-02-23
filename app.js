e1fc7a9c08386328be92a653c2aa7a1cc11ec16a
tronghuyy <binhellboy098@gmail.com>
2025-02-23 17:22:20 +0700

    huy2

--- app.js
+++ app.js
@@ -6,28 +6,24 @@ const port = 3000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var productsRouter = require('./routes/products');
+var indexADMIN = require('./routes/admin/indexADMIN');
+var productdetail = require('./routes/productdetail');
+var cartRouter = require('./routes/cart');
 var app = express();
 // Cấu hình thư mục 'public' làm thư mục tĩnh
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/client'),path.join(__dirname, 'views/admin')]);
 app.set('view engine', 'ejs');
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
+app.use('/products', productsRouter);
+app.use('/products', productdetail);
+app.use('/cart', cartRouter);
+app.use('/admin', indexADMIN);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
 module.exports = app;
