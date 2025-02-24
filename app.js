32e7174caf4ba2373f379da453140c5671aace5e
boden1501 <htvinh1501@gmail.com>
2025-02-24 15:01:15 +0700

    Vinh

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 // Cấu hình thư mục 'public' làm thư mục tĩnh
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
 
@@ -20,11 +20,11 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
